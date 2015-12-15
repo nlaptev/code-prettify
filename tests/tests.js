ed846d39e7dffb7c7f99749548a589271cc7e0f3
nlaptev <nlaptev@yahoo-inc.com>
2015-12-14 20:19:32 -0800

    basic tests

--- tests/tests.js
+++ tests/tests.js
@@ -26,34 +26,32 @@ describe('prettify_test.html', function () {
                "src/lang-vb.js",
                "src/lang-yaml.js"],
               { features : {
-                FetchExternalResources   : ['script'],
-                ProcessExternalResources : ['script'],
-                MutationEvents           : '2.0',
-                QuerySelector            : false
-              },
-              done : function (error, window) {
-               w = window;
-               goldens = window.goldens;
-               window.prettyPrint("", window.document);
-               done();
-              }
-    });    
+                  FetchExternalResources   : ['script'],
+                  ProcessExternalResources : ['script'],
+                  MutationEvents           : '2.0',
+                  QuerySelector            : false
+                },
+                done : function (error, window) {
+                  w = window;
+                  goldens = window.goldens;
+                  window.prettyPrint("", window.document);
+                  done();
+                }
+              });    
   });
   
   it('doStuff()', function() {
-      for (var lang in goldens) {
-            var container = w.document.getElementById(lang);
-            // Convert abbreviations that start with `.
-            var golden = goldens[lang].replace(/`([A-Z]{3})/g, function (_, lbl) {
-                return (lbl == 'END'
-                    ? '<\/span>'
-                    : '<span class="' + lbl.toLowerCase() + '">');
-              })
-              // Line numbers
-              .replace(/`#(?![0-9])/, '<li class="L0">')
-              .replace(/`#([0-9])/g, '</li><li class="L$1">');
-            var actual = container.innerHTML;
-            assert.equal(golden, actual);
-      }
+    for (var lang in goldens) {
+      var container = w.document.getElementById(lang);
+      // Convert abbreviations that start with `.
+      var golden = goldens[lang].replace(/`([A-Z]{3})/g, function (_, lbl) {
+        return (lbl == 'END' ? '<\/span>' : '<span class="' + lbl.toLowerCase() + '">');
+        })
+        // Line numbers
+        .replace(/`#(?![0-9])/, '<li class="L0">')
+        .replace(/`#([0-9])/g, '</li><li class="L$1">');
+      var actual = container.innerHTML;
+      assert.equal(golden, actual);
+    }
   });
 });
